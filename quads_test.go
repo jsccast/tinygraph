06522649487d7bc2b3e5a6f827c2203dcd7e13a0
Jamie Stephens <james_stephens@cable.comcast.com>
2014-10-16 13:13:57 -0500

    Quads test.

---
+++ quads_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestQuads(t *testing.T) {
+	triple, err := ParseTriple(`<http://wordnet-rdf.princeton.edu/wn31/100003553-n> <http://wordnet-rdf.princeton.edu/ontology#part_holonym> <http://wordnet-rdf.princeton.edu/wn31/103898588-n> .
+`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(triple.ToString())
+}
